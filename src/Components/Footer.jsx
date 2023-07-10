d9f89cc7f0d1d68b91c474b5e0fcd2d85d247a23
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2023-07-10 12:19:28 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer className=' dark bg-slate-800 bg-black p-2.5 flex justify-center items-center'>
       <BsGithub className="mx-2" />
-      <p>GitHub Finder | © 2021 Shubham maurya</p>
+      <p>GitHub Finder | © 2023 Shubham maurya</p>
     </footer>
   );
 };
